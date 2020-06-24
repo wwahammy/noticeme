416afd4edf00ef78fd066eff96f261d0d8611988
Eric Schultz <eric@commitchange.com>
2020-06-24 15:17:14 -0500

    Initial commit

---
+++ noticeme.js
@@ -0,0 +1,44 @@
+// License: LGPL-3.0-or-later
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const readPkgTree = require('read-package-tree');
+const fetch = require('node-fetch');
+const fs = require('fs')
+const pathMod = require('path')
+
+const npmjsCoordinates = ({ name, version }) =>
+  'npm/npmjs/' + (name.includes('/') ? name : `-/${name}`) + `/${version}`;
+
+function retrieveIncludedJson(path, file = 'included.json') {
+  JSON.parse(fs.readFileSync(pathMod.join(path, file), 'utf8')).packages
+}
+
+module.exports = function noticeme(path, rpt = readPkgTree, http = fetch) {
+  return new Promise((resolve, reject) => {
+    rpt(path, function (err, { children }) {
+      if (err) reject(err);
+
+      const pkgJsonLicenses = new Map();
+      let coordinates = children.map(({ package, children: more }) => {
+        children.push(...more);
+        const coordinate = npmjsCoordinates(package);
+
+        // TODO: make use of these as fallback
+        pkgJsonLicenses.set(coordinate, package.license);
+
+        return coordinate;
+      });
+
+      retrieveIncludedJson(path)
+
+      http('https://api.clearlydefined.io/notices', {
+        method: 'post',
+        body: JSON.stringify({ coordinates }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then(res => res.json())
+        .then(json => resolve(json.content));
+    });
+  });
+}
